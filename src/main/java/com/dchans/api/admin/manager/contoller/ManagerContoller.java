2d21a571e5305f78753f76dc70d29cbc019e88a4
Chans <ahwkt46@gmail.com>
2025-07-06 23:35:11 +0900

    admin 패키지 생성

--- src/main/java/com/dchans/api/admin/manager/contoller/ManagerContoller.java
+++ src/main/java/com/dchans/api/admin/manager/contoller/ManagerContoller.java
@@ -1,9 +1,18 @@
 package com.dchans.api.admin.manager.contoller;
 
+import com.dchans.api.admin.common.ApiResponse;
+import com.dchans.api.admin.manager.dto.ManagerDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ManagerContoller {
 
-
+    @GetMapping("/users/{id}")
+    public ApiResponse<ManagerDto> getUser(@PathVariable Long id) {
+        //ManagerDto user = userService.findById(id);
+        //return ApiResponse.success(user);
+        return null;
+    }
 }
